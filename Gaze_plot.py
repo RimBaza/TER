387fdc4bfd6e7d1bddc219561ea028187b1d09de
RimBaza <138826543+RimBaza@users.noreply.github.com>
2024-03-22 15:08:29 +0100

    Gaze_plot.py

--- Gaze_plot.py
+++ Gaze_plot.py
@@ -1,21 +1,18 @@
 import pandas as pd
 from math import sqrt
 
-# Load the dataset
 df = pd.read_csv('GazeRV_ON26.csv')
 
 
-# Function to calculate the distance between two points
 def calculate_distance(x1, y1, x2, y2):
     return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 def calculate_mean_distance_per_time(total_distances_percentage, total_time):
-    # Convert total times to seconds if they are in milliseconds
-    total_times_in_seconds = total_time / 1000.0  # Adjust this line based on your data's time units
+   
+    total_times_in_seconds = total_time / 1000.0  
     mean_distance_per_time = total_distances_percentage / total_times_in_seconds
     return mean_distance_per_time
 
-# Prepare a list to collect summary data for all participants
 summary_data = []
 
 for participant_id in df['IDParticipant'].unique():
@@ -43,26 +40,19 @@ for participant_id in df['IDParticipant'].unique():
     if last_valid_point is not None and 0 <= participant_data.iloc[-1]['ScreenX'] <= 1 and 0 <= participant_data.iloc[-1]['ScreenY'] <= 1:
         total_time += participant_data.iloc[-1]['Time'] - last_valid_point['Time']
 
-
-    # Assuming total_distance_percentage and total_time_percentage are calculated as before
     mean_distance_per_time_percentage = calculate_mean_distance_per_time(total_distance, total_time)
 
-    # Append the collected information for the current participant to the summary list
     summary_data.append({
         'Participant ID': participant_id,
         'Total Distance': total_distance,
         'Total Time in ms': total_time,
         'mean_distance_per_sec' : mean_distance_per_time_percentage,
-        'Path Count': path_count + 1  # Adjust for the last path
+        'Path Count': path_count + 1
     })
 
 
-
-# Convert the summary list to a DataFrame
 summary_df = pd.DataFrame(summary_data)
 
-# Set the Participant ID as the index (optional)
 summary_df.set_index('Participant ID', inplace=True)
 
-# Print the DataFrame as a table
 print(summary_df)
